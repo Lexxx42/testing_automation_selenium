c14410276eb85c537312ae03820caa778329f643
Alexander Konukhov <jesusmisha@gmail.com>
2023-02-03 15:48:44 +0300

    install Windows driver

---
+++ 2_start_browser_by_WebDriver/3_windows_install.py
@@ -0,0 +1,14 @@
+# https://stepik.org/lesson/25969/step/8?auth=login&unit=196192
+
+# Для установки откройте сайт https://sites.google.com/chromium.org/driver/
+# и скачайте ту версию ChromeDriver, которая соответствует версии вашего браузера.
+# Чтобы узнать версию браузера, откройте новое окно в Chrome,
+# в поисковой строке наберите: chrome://version/ и нажмите Enter.
+# В верхней строчке вы увидите информацию про версию браузера.
+
+# Скачайте с сайта https://sites.google.com/chromium.org/driver/ драйвер для вашей версии браузера.
+# Разархивируйте скачанный файл.
+# Создайте на диске C: папку chromedriver и положите разархивированный
+# ранее файл chromedriver.exe в папку C:\chromedriver.
+# Добавьте в системную переменную PATH папку C:\chromedriver.
+# Как это сделать в разных версиях Windows, описано здесь: https://www.computerhope.com/issues/ch000549.htm.
