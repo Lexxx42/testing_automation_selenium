902a17279615fbcc2e08ad24ab617f6542e41978
Alexandr Konukhov <jesusmisha@gmail.com>
2023-02-28 15:49:11 +0300

    fix error

--- 1_Introduction_to_selenium/6_element_search_by_SeleniumWebDriver/10_unique_selectors.py
+++ 1_Introduction_to_selenium/6_element_search_by_SeleniumWebDriver/10_unique_selectors.py
@@ -3,14 +3,17 @@ from selenium import webdriver
 from selenium.webdriver.common.by import By
 
 try:
-    LINK = "http://suninjuly.github.io/registration2.html"
+    # With a bug.
+    # LINK = "http://suninjuly.github.io/registration2.html"
+    # W/o a bug.
+    LINK = "http://suninjuly.github.io/registration1.html"
     browser = webdriver.Chrome()
     browser.get(LINK)
 
     # Ваш код, который заполняет обязательные поля
-    browser.find_element(By.CSS_SELECTOR, 'input[placeholder="Input your first name"]').send_keys("Ivan")
-    browser.find_element(By.CSS_SELECTOR, 'input[placeholder="Input your last name"]').send_keys("Petrov")
-    browser.find_element(By.CSS_SELECTOR, 'input[placeholder="Input your email"]').send_keys("ss@asd.sa")
+    browser.find_element(By.CSS_SELECTOR, ".first_block .form-control.second").send_keys("Ivan")
+    browser.find_element(By.CSS_SELECTOR, ".first_block .form-control.first").send_keys("Petrov")
+    browser.find_element(By.CSS_SELECTOR, ".first_block .form-control.third").send_keys("ss@asd.sa")
 
     # Отправляем заполненную форму
     button = browser.find_element(By.CSS_SELECTOR, "button.btn")
