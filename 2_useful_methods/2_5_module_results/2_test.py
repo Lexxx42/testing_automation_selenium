f6e2cd64bf2e410105ea3b0234a7789b2d2a2082
Alexandr Konukhov <jesusmisha@gmail.com>
2023-02-28 11:51:43 +0300

    module summary

---
+++ 2_useful_methods/2_5_module_results/2_test.py
@@ -0,0 +1,12 @@
+# Задание: работа с alert
+# Отметьте синтаксически верный способ переключиться на alert-сообщение в браузере.
+
+# Выберите один вариант из списка
+
+# 1. browser.switch_to.window
+# 2. browser.switch_to.alert()
+# 3. browser.switch_to.alert
+# 4. browser.switch_to_alert
+# 5. browser.window_handles[1]
+
+# 3
---
+++ 2_useful_methods/2_5_module_results/6_ex.py
@@ -0,0 +1,11 @@
+# Expected conditions
+# Изучите самостоятельно страницу с методами для явных ожиданий Expected Conditions.
+
+# Как называется условие, которое проверяет,
+# что внутри элемента появился определенный текст?
+# Введите название ниже без скобок, точек и экземпляра драйвера.
+# Только название.
+
+# https://selenium-python.readthedocs.io/api.html#module-selenium.webdriver.support.expected_conditions
+
+# text_to_be_present_in_element
