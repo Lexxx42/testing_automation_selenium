e249069205305fe0bdd4ee0bcc4d2fb993b3a4f2
Alexander Konukhov <jesusmisha@gmail.com>
2023-03-04 16:40:17 +0300

    The owls are not what they seem! OvO

--- 3_test_frameworks/3_6_parameterization/4_ex_test.py
+++ 3_test_frameworks/3_6_parameterization/4_ex_test.py
@@ -16,37 +16,45 @@ link = 'https://stepik.org/lesson/236895/step/1'
 
 
 def test_login(browser):
-    try:
-        browser.implicitly_wait(5)
-        browser.get(link)
-        login_button = WebDriverWait(browser, 10).until(
-            EC.visibility_of_element_located((By.CSS_SELECTOR, '#ember33'))
-        )
-        login_button.click()
+    browser.get(link)
+    time.sleep(3)
+    login_button = WebDriverWait(browser, 10).until(
+        EC.visibility_of_element_located((By.CSS_SELECTOR, '#ember33'))
+    )
+    login_button.click()
 
-        login_email = browser.find_element(By.CSS_SELECTOR, '#id_login_email')
-        login_email.send_keys(LOGIN)
+    login_email = browser.find_element(By.CSS_SELECTOR, '#id_login_email')
+    login_email.send_keys(LOGIN)
 
-        login_pass = browser.find_element(By.CSS_SELECTOR, '#id_login_password')
-        login_pass.send_keys(PASS)
+    login_pass = browser.find_element(By.CSS_SELECTOR, '#id_login_password')
+    login_pass.send_keys(PASS)
 
-        browser.find_element(By.CSS_SELECTOR, '.sign-form__btn').click()
+    browser.find_element(By.CSS_SELECTOR, '.sign-form__btn').click()
+    time.sleep(5)
 
-        text_area = WebDriverWait(browser, 10).until(
-            EC.presence_of_element_located((By.CSS_SELECTOR, 'textarea'))
-        )
-        text_area.clear()
-        text_area.send_keys(str(log(int(time.time()))))
+    if len(do_again := browser.find_elements(By.CSS_SELECTOR, '.again-btn')) > 0:
+        do_again[0].click()
+        time.sleep(1)
 
-        submit_button = WebDriverWait(browser, 10).until(
-            EC.element_to_be_clickable((By.CSS_SELECTOR, '.submit-submission'))
-        )
-        submit_button.click()
+    text_area = WebDriverWait(browser, 10).until(
+        EC.element_to_be_clickable((By.CSS_SELECTOR, 'textarea'))
 
-        tip = WebDriverWait(browser, 10).until(
-            EC.presence_of_element_located((By.CSS_SELECTOR, '.smart-hints__hint'))
-        )
-        tip_text = tip.text
+    )
 
-    finally:
-        assert tip_text in 'Correct!'
+    text_area.clear()
+    time.sleep(2)
+    text_area.send_keys(str(log(int(time.time()))))
+
+    submit_button = WebDriverWait(browser, 10).until(
+        EC.element_to_be_clickable((By.CSS_SELECTOR, '.submit-submission'))
+    )
+    submit_button.click()
+
+    time.sleep(2)
+
+    tip = WebDriverWait(browser, 10).until(
+        EC.visibility_of_element_located((By.CSS_SELECTOR, '.smart-hints__hint'))
+    )
+    time.sleep(2)
+    tip_text = tip.text
+    assert tip_text in 'Correct!'
--- 3_test_frameworks/3_6_parameterization/4_ex_test_parameterization.py
+++ 3_test_frameworks/3_6_parameterization/4_ex_test_parameterization.py
@@ -60,42 +60,52 @@ LOGIN = os.getenv('LOGIN')
 PASS = os.getenv('PASWD')
 
 link = ['https://stepik.org/lesson/236895/step/1', 'https://stepik.org/lesson/236896/step/1',
-         'https://stepik.org/lesson/236897/step/1', 'https://stepik.org/lesson/236898/step/1',
-         'https://stepik.org/lesson/236899/step/1', 'https://stepik.org/lesson/236903/step/1',
-         'https://stepik.org/lesson/236904/step/1', 'https://stepik.org/lesson/236905/step/1']
+        'https://stepik.org/lesson/236897/step/1', 'https://stepik.org/lesson/236898/step/1',
+        'https://stepik.org/lesson/236899/step/1', 'https://stepik.org/lesson/236903/step/1',
+        'https://stepik.org/lesson/236904/step/1', 'https://stepik.org/lesson/236905/step/1']
+
 
 @pytest.mark.parametrize('link', link)
 def test_login(browser, link):
-    try:
-        browser.get(link)
-        login_button = WebDriverWait(browser, 10).until(
-            EC.visibility_of_element_located((By.CSS_SELECTOR, '#ember33'))
-        )
-        login_button.click()
+    browser.get(link)
+    time.sleep(3)
+    login_button = WebDriverWait(browser, 10).until(
+        EC.visibility_of_element_located((By.CSS_SELECTOR, '#ember33'))
+    )
+    login_button.click()
 
-        login_email = browser.find_element(By.CSS_SELECTOR, '#id_login_email')
-        login_email.send_keys(LOGIN)
+    login_email = browser.find_element(By.CSS_SELECTOR, '#id_login_email')
+    login_email.send_keys(LOGIN)
 
-        login_pass = browser.find_element(By.CSS_SELECTOR, '#id_login_password')
-        login_pass.send_keys(PASS)
+    login_pass = browser.find_element(By.CSS_SELECTOR, '#id_login_password')
+    login_pass.send_keys(PASS)
 
-        browser.find_element(By.CSS_SELECTOR, '.sign-form__btn').click()
+    browser.find_element(By.CSS_SELECTOR, '.sign-form__btn').click()
+    time.sleep(5)
 
-        text_area = WebDriverWait(browser, 10).until(
-            EC.presence_of_element_located((By.CSS_SELECTOR, 'textarea'))
-        )
-        text_area.clear()
-        text_area.send_keys(str(log(int(time.time()))))
+    if len(do_again := browser.find_elements(By.CSS_SELECTOR, '.again-btn')) > 0:
+        do_again[0].click()
+        time.sleep(1)
 
-        submit_button = WebDriverWait(browser, 10).until(
-            EC.element_to_be_clickable((By.CSS_SELECTOR, '.submit-submission'))
-        )
-        submit_button.click()
+    text_area = WebDriverWait(browser, 10).until(
+        EC.element_to_be_clickable((By.CSS_SELECTOR, 'textarea'))
 
-        tip = WebDriverWait(browser, 10).until(
-            EC.presence_of_element_located((By.CSS_SELECTOR, '.smart-hints__hint'))
-        )
-        tip_text = tip.text
+    )
 
-    finally:
-        assert tip_text in 'Correct!'
+    text_area.clear()
+    time.sleep(2)
+    text_area.send_keys(str(log(int(time.time()))))
+
+    submit_button = WebDriverWait(browser, 10).until(
+        EC.element_to_be_clickable((By.CSS_SELECTOR, '.submit-submission'))
+    )
+    submit_button.click()
+
+    time.sleep(2)
+
+    tip = WebDriverWait(browser, 10).until(
+        EC.visibility_of_element_located((By.CSS_SELECTOR, '.smart-hints__hint'))
+    )
+    time.sleep(2)
+    tip_text = tip.text
+    assert tip_text in 'Correct!'
